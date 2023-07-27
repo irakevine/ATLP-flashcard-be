d1328434737ed143ae2292dd237a93db58253ff4
irakevine <ikevine2016@gmail.com>
2023-07-27 07:34:00 +0200

    creating index.js with server connection

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,44 @@
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import http from "http";
 
-console.log("hello World");
+import {
+	ApolloServerPluginLandingPageGraphQLPlayground
+  } from "apollo-server-core";
+
+
+const IS_DEV = process.env.NODE_ENV === "development";
+const localOrigins = [/^http:\/\/localhost:\d{4}$/];
+const prodOrigins = [/^https:\/\/.*\.yourdomain\.com$/];
+
+async function startApolloServer() {
+	const app = express();
+	const httpServer = http.createServer(app);
+	const server = new ApolloServer({
+		
+		plugins: [
+			ApolloServerPluginDrainHttpServer({ httpServer }),
+			ApolloServerPluginLandingPageGraphQLPlayground()
+		],
+	});
+	await server.start();
+
+	
+
+	server.applyMiddleware({ 
+		app,
+		cors: {
+			origin: IS_DEV ? localOrigins : prodOrigins,
+			credentials: true,
+		},
+	});
+	await new Promise<void>((resolve) => {
+        httpServer.listen({ port: 4000 })
+        resolve()
+    });
+	console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+// Start server
+startApolloServer()
